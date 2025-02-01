4b4b77c9fc64419ae7c35ef72ec1e8f3cb2e498f
sush1999ingole6925 <sush1999ingole6925@gmail.com>
2025-02-02 01:50:08 +0530

    Added links for home page button

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,8 +8,12 @@ const Home = () => {
                     <span className="title1">HI, I'M SUSHIL</span><br />
                     <span className="title2">A WEB DEVELOPER</span><br />
                     <span className="title3">Focused on creating seamless, user-friendly, and visually appealing web experiences that captivate users and meet business goals.</span>
-                    <button type="button" className="btn btn-outline-dark dwnld-cv-btn">Download CV</button>
-                    <button type="button" className="btn btn-dark contact-btn">Contact Info</button>
+                    <a href="/Sushil_Ingole_Resume_2024__MEAN.pdf" download="Sushil_CV.pdf">
+                        <button type="button" className="btn btn-outline-dark dwnld-cv-btn">Download CV</button>
+                    </a>
+                    <a href="tel:+917057879165">
+                        <button type="button" className="btn btn-dark contact-btn">Contact Info</button>
+                    </a>
                 </div>
                 <div className="right-section">
                     <img src="sushil-photo.jpg" alt="Photo" />
